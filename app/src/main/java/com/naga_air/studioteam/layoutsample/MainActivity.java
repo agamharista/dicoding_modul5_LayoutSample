af3672cd776965d2896dc6ef8c8a67efc395d07b
Agam <agammail95@gmail.com>
2017-03-30 16:01:38 +0700

    Merapikan teks dan menambahkan gambar

--- app/src/main/java/com/naga_air/studioteam/layoutsample/MainActivity.java
+++ app/src/main/java/com/naga_air/studioteam/layoutsample/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setTitle("Hotel Hilton");
-        getSupportActionBar().setSubtitle("Isla Nublar");
+        getSupportActionBar().setTitle("Hotel Fave");
+        getSupportActionBar().setSubtitle("Melawai, Jakarta");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
